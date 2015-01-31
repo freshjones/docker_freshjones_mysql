8d709067575a766a624ebe56588e27fe87e7f738
William Jones <billy@freshjones.com>
2015-01-31 13:22:11 -0500

    adjusting for random passwords

--- scripts/mysql_admin_user.sh
+++ scripts/mysql_admin_user.sh
@@ -10,11 +10,14 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
-_word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
-echo "=> Creating MySQL admin user with ${_word} password"
+ROOTPASS= $(pwgen -s 12 1)
+ADMINPASS=$(pwgen -s 12 1)
 
-mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
+mysqladmin -u root password $ROOTPASS
+
+echo "=> Creating MySQL admin user with random password"
+
+mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$ADMINPASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 
 echo "=> Done!"
@@ -22,10 +25,12 @@ echo "=> Done!"
 echo "========================================================================"
 echo "You can now connect to this MySQL Server using:"
 echo ""
-echo "    mysql -uadmin -p$PASS -h<host> -P<port>"
+echo "    mysql -uadmin -p$ADMINPASS -h<host> -P<port>"
 echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "MySQL user 'root' has no password but only allows local connections"
 echo "========================================================================"
 
-mysqladmin -uroot shutdown
+echo "Root pwd is $ROOTPASS"
+
+mysqladmin -uroot -p$ROOTPASS shutdown
